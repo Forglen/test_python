8f05d532c92c62362d7a118fd59735ce8f98ff54
GLNC-liyunliang <1393104812@qq.com>
2021-01-23 11:06:56 +0800

    second commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ??
+@author: liyunliang
+@license: Apache Licence 
+@file: demo.py
+@time: 2021/1/23 9:49
+"""
+
+print(1)
+
+if __name__ == '__main__':
+    pass
