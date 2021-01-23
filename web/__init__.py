b13a30c45f916eafe7c1f5f324d40edde4ee5a6f
GLNC-liyunliang <1393104812@qq.com>
2021-01-23 09:47:58 +0800

    first commit

---
+++ web/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@version: ??
+@author: liyunliang
+@license: Apache Licence 
+@file: __init__.py.py
+@time: 2021/1/23 9:46
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
