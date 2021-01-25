eaec8765367b4fcd21d6a567f058875f82650b98
Your Name <you@example.com>
2021-01-25 20:54:34 +0800

    f commit

---
+++ web/test_pytest.py
@@ -0,0 +1,19 @@
+"""
+@version: ??
+@author: liyunliang
+@license: Apache Licence 
+@file: test_pytest.py 
+@time: 2021/1/23 11:58 
+"""
+import pytest
+class Calc:
+    def add(self):
+
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calc()
+
+    def test_add(self):
+
